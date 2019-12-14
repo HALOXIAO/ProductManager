26156f5ad08b7799e104a079d67cac40de4553db
HALOXIAO <584714929@qq.com>
2019-12-14 21:33:12 +0800

    ··

--- src/main/java/com/manage/sys/ManageApplication.java
+++ src/main/java/com/manage/sys/ManageApplication.java
@@ -1,11 +1,13 @@
 package com.manage.sys;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableRedisHttpSession
+@MapperScan("com.manage.sys.DAO")
 public class ManageApplication {
 
     public static void main(String[] args) {
