b2616e34cbd5e2e08c908ecb466e0b8b271bdaba
HALOXIAO <584714929@qq.com>
2019-12-17 09:45:54 +0800

    log

--- src/main/java/com/manage/sys/manager/common/log/LogAspect.java
+++ src/main/java/com/manage/sys/manager/common/log/LogAspect.java
@@ -1,8 +1,10 @@
-package com.manage.sys.Manager.common.log;
+package com.manage.sys.manager.common.log;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 @Aspect
@@ -11,5 +13,7 @@ public class LogAspect {
 
     @Before("execution(* com.manage.sys.controller..*.*(..))")
     public void test() {
+        Logger logger = LoggerFactory.getLogger(Object.class);
+        
     }
 }
