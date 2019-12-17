9b7af2c24328e8880637ea7407234eb1953b4e27
Hao-Warming <53370100+Hao-Warming@users.noreply.github.com>
2019-12-17 21:42:00 +0800

    service

--- src/main/java/com/manage/sys/service/impl/EmployeeService.java
+++ src/main/java/com/manage/sys/service/impl/EmployeeService.java
@@ -1,7 +1,23 @@
 package com.manage.sys.service.impl;
 
+import com.manage.sys.entity.BO.EmployeeBO;
+import com.manage.sys.entity.PO.EmployeePO;
 import com.manage.sys.service.EmployeeServiceInterface;
 
 public class EmployeeService implements EmployeeServiceInterface {
 
+    @Override
+    public void AddEmployee(EmployeePO employee){
+
+    }
+
+    @Override
+    public void SelectEmployee(EmployeePO employee){
+
+    }
+
+    @Override
+    public void UpdateEmployee(EmployeePO employee){
+
+    }
 }
