56bc4a1b684476d3f9d26ecf7f753bb27f0b7ad4
Hao-Warming <53370100+Hao-Warming@users.noreply.github.com>
2019-12-18 18:04:18 +0800

    emp

--- src/main/java/com/manage/sys/service/EmployeeService.java
+++ src/main/java/com/manage/sys/service/EmployeeService.java
@@ -1,9 +1,13 @@
 package com.manage.sys.service;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.manage.sys.dao.condition.UpdateCondition;
 import com.manage.sys.dao.wrapper.impl.EmployeeWrapper;
+import com.manage.sys.entity.PO.EmployeePO;
 import com.manage.sys.entity.PO.RolePO;
+import com.manage.sys.entity.PO.UserPO;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
 
 
 public class EmployeeService {
@@ -11,11 +15,27 @@ public class EmployeeService {
     @Autowired
     EmployeeWrapper employeeWrapper;
 
+    @Autowired
+    UserDetailsByNameServiceWrapper userDetailsByNameServiceWrapper;
+
     public void searchEmployeeByID(int id){
         employeeWrapper.searchEmployeeById(id);
     }
-    public boolean addRole(RolePO rolePO){
-        return employeeWrapper.addEmployee(rolePO)
+
+    public boolean addEmployee(EmployeePO employeePO){
+        return employeeWrapper.addEmployee(employeePO);
+    }
+
+    public boolean updateEmployee(EmployeePO employeePO){
+        employeeWrapper.updateEmployee(employeePO);
+        boolean flag1=employeeWrapper.updateEmployee(employeePO);
+        if(employeePO.getStatus()!=null){
+            UpdateCondition<Integer,UserPO> updateCondition = new UpdateCondition<>();
+            UpdateWrapper<UserPO> wrapper = updateCondition.updateEmployeeBy("status",employeePO.getStatus());
+            boolean flag2 =
+            return flag1 && flag2;
+        }
+        return flag1;
     }
 
 }
