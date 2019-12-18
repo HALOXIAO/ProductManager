4f40faa77881f406c9db64e33f63392977d3778c
HALOXIAO <584714929@qq.com>
2019-12-18 15:12:27 +0800

    logic

--- src/main/java/com/manage/sys/ManageApplication.java
+++ src/main/java/com/manage/sys/ManageApplication.java
@@ -3,9 +3,10 @@ package com.manage.sys;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.manage.sys.dao")
+@ComponentScan("com.manage.sys.dao")
 public class ManageApplication {
 
     public static void main(String[] args) {
