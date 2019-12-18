a38df43bf7b249508f9f0da5f6d9212746452d3f
HALOXIAO <584714929@qq.com>
2019-12-18 08:08:32 +0800

    password

---
+++ src/main/java/com/manage/sys/config/EmailController.java
@@ -0,0 +1,11 @@
+package com.manage.sys.config;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Email")
+public class EmailController {
+
+
+}
